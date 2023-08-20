822830f6de5cd8a52e306228e9a2525d65729d25
jrdnwlkr <walker.jordan.m@gmail.com>
2023-08-20 12:55:44 -0500

    webpack compiled successfully

--- Develop/client/webpack.config.js
+++ Develop/client/webpack.config.js
@@ -27,23 +27,11 @@ module.exports = () => {
         description: 'My awesome Progressive Web App!',
         background_color: '#ffffff',
         crossorigin: 'use-credentials', //can be null, use-credentials or anonymous
-        icons: [
-          {
-            src: path.resolve('src/assets/icon.png'),
-            sizes: [96, 128, 192, 256, 384, 512] // multiple sizes
-          },
-          {
-            src: path.resolve('src/assets/large-icon.png'),
-            size: '1024x1024' // you can also use the specifications pattern
-          },
-          {
-            src: path.resolve('src/assets/maskable-icon.png'),
-            size: '1024x1024',
-            purpose: 'maskable'
-          }
-        ]
       }),
-      
+      new InjectManifest({
+        swSrc: './src-sw.js', // Path to your service worker source file
+        // Other options here...
+      }),
     ],
 
     module: {
